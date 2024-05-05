452d5f567793daf5411a621712f5fb940d68603a
Yukihira2k <glen.mirandilla02@gmail.com>
2024-05-06 04:12:25 +0800

    fixed mobile navBar bug

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -45,8 +45,8 @@ const NavBar = () => {
         {nav&&(
 
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[#0C0F11]">
-        {links.map(({id,link}) =>(
-        <li key={id}className="px-4 cursor-pointer capitalize py-6 text-4xl"><Link to={link} smooth duration={500} onClick={()=> setNav(!nav)}>{link}</Link></li>
+        {links.map(({id,link,linkName}) =>(
+        <li key={id}className="px-4 cursor-pointer capitalize py-6 text-4xl"><Link to={link} smooth duration={500} onClick={()=> setNav(!nav)}>{linkName}</Link></li>
         ))}
 
 </ul>
